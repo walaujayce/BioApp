714c0e042a7bfaa493ff7ee566d91063cb2012fc
Jayce <jayce@uneotech.com>
2025-11-30 15:17:11 +0800

    -renew android folder and pubspec.yaml

--- Biological_app/BioApp-Flutter/bio_app/lib/pages/login_page.dart
+++ Biological_app/BioApp-Flutter/bio_app/lib/pages/login_page.dart
@@ -77,7 +77,7 @@ class LoginPage extends HookConsumerWidget {
     return FlutterLogin(
       title: "中油生態地圖",
       onLogin: login,
-      onSignup: signUp,
+      // onSignup: signUp,
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) => HomePage(),
@@ -99,16 +99,16 @@ class LoginPage extends HookConsumerWidget {
           signupButton: '註冊',
           goBackButton: '返回',
           confirmPasswordError: '密碼不一致！',
-          emailHint: "電子郵件地址",
-          emailError: "請輸入電子郵件！",
-          legalNameHint: "姓名",
-          legalNameError: "請輸入正確的姓名！",
-          phoneHint: "電話號碼",
-          phoneError: "請輸入正確的電話號碼（含國際碼）！",
+          // emailHint: "電子郵件地址",
+          // emailError: "請輸入電子郵件！",
+          // legalNameHint: "姓名",
+          // legalNameError: "請輸入正確的姓名！",
+          // phoneHint: "電話號碼",
+          // phoneError: "請輸入正確的電話號碼（含國際碼）！",
           signUpSuccess: "註冊成功！請至電子郵件信箱驗證您的帳號！",
-          signUpMessage: "本 App 只能拿來拍攝動植物，如果是非生物或人像，將會刪除該筆資料",
-          acceptButtonText: "同意",
-          declineButtonText: "不同意",
+          // signUpMessage: "本 App 只能拿來拍攝動植物，如果是非生物或人像，將會刪除該筆資料",
+          // acceptButtonText: "同意",
+          // declineButtonText: "不同意",
           forgotPasswordButton: "忘記密碼？",
           recoverPasswordButton: "重設密碼",
           recoverPasswordIntro: "請輸入您的電子郵件地址",
@@ -119,4 +119,14 @@ class LoginPage extends HookConsumerWidget {
       loginAfterSignUp: false,
     );
   }
+}
+
+extension on LoginData {
+  get email => null;
+
+  get legalName => null;
+}
+
+extension on LoginMessages{
+  get emailHint => null;
 }
--- Biological_app/BioApp-Flutter/bio_app/lib/utils/api_caller.dart
+++ Biological_app/BioApp-Flutter/bio_app/lib/utils/api_caller.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
-import '../env.dart';
+import '../env.example.dart';
 
 class ApiResponse {
   late http.Response response;
